64673c0eaaf5e85ec6faeea522ebda9ae0ab2e94
Farhan160200 <fadhlurrahmanfarhan16@gmail.com>
2022-05-22 23:49:14 +0700

    added chucker and implementation build variants

--- app/src/main/java/com/farhanfarkaann/challenge5/MainActivity.kt
+++ app/src/main/java/com/farhanfarkaann/challenge5/MainActivity.kt
@@ -2,10 +2,20 @@ package com.farhanfarkaann.challenge5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(BuildConfig.FLAVOR.equals("demo")) {
+            Toast.makeText(this, "ini adlaah demo", Toast.LENGTH_SHORT).show()
+        }else if(BuildConfig.FLAVOR.equals("full")) {
+            Toast.makeText(this, "ini adlaah full", Toast.LENGTH_SHORT).show()
+
+
+        }
     }
 }
